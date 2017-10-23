bd3c20d79f290032bc779d50e20baa246daff64a
Stephen Hill <sjh199@zepler.org>
2017-10-22 18:06:33 -0700

    More whitespace

--- webgl/shaders/ltc/ltc_blit.fs
+++ webgl/shaders/ltc/ltc_blit.fs
@@ -40,15 +40,15 @@ vec3 saturate(vec3 v)
 
 vec3 aces_fitted(vec3 color)
 {
-	mat3 ACES_INPUT_MAT = mat3_from_rows(
-	    vec3( 0.59719, 0.35458, 0.04823),
-	    vec3( 0.07600, 0.90834, 0.01566),
-	    vec3( 0.02840, 0.13383, 0.83777));
+    mat3 ACES_INPUT_MAT = mat3_from_rows(
+        vec3( 0.59719, 0.35458, 0.04823),
+        vec3( 0.07600, 0.90834, 0.01566),
+        vec3( 0.02840, 0.13383, 0.83777));
 
-	mat3 ACES_OUTPUT_MAT = mat3_from_rows(
-	    vec3( 1.60475,-0.53108,-0.07367),
-	    vec3(-0.10208, 1.10813,-0.00605),
-	    vec3(-0.00327,-0.07276, 1.07602));
+    mat3 ACES_OUTPUT_MAT = mat3_from_rows(
+        vec3( 1.60475,-0.53108,-0.07367),
+        vec3(-0.10208, 1.10813,-0.00605),
+        vec3(-0.00327,-0.07276, 1.07602));
 
     color = mul(ACES_INPUT_MAT, color);
 
@@ -79,13 +79,13 @@ void main()
 {
     vec2 pos = gl_FragCoord.xy/resolution;
 
-	vec4 col = texture(tex, pos);
+    vec4 col = texture(tex, pos);
 
-	// Rescale by number of samples
-	col /= col.w;
+    // Rescale by number of samples
+    col /= col.w;
 
-	col.rgb = aces_fitted(col.rgb);
-	col.rgb = ToSRGB(col.rgb);
+    col.rgb = aces_fitted(col.rgb);
+    col.rgb = ToSRGB(col.rgb);
 
     FragColor = vec4(col);
 }
