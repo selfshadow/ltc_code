bc9a160f02e3b9c36d2283cd7063cb21f63e76fa
Stephen Hill <sjh199@zepler.org>
2017-10-24 15:33:20 -0700

    Directly generate RGB9_E5 pngs

--- prefilter/prefilterAreaLightwithBorder.cpp
+++ prefilter/prefilterAreaLightwithBorder.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <sstream>
 #include <iomanip>
+#include <cstdint>
 using namespace std;
 
 
@@ -177,7 +178,141 @@ void filterWithBorder(CImg<float>& imageInput, CImg<float>& imageOutput, const i
     return;
 }
 
+// Adapted from https://www.khronos.org/registry/OpenGL/extensions/EXT/EXT_texture_shared_exponent.txt
+////////
+#define RGB9E5_EXPONENT_BITS          5
+#define RGB9E5_MANTISSA_BITS          9
+#define RGB9E5_EXP_BIAS               15
+#define RGB9E5_MAX_VALID_BIASED_EXP   31
 
+#define MAX_RGB9E5_EXP               (RGB9E5_MAX_VALID_BIASED_EXP - RGB9E5_EXP_BIAS)
+#define RGB9E5_MANTISSA_VALUES       (1<<RGB9E5_MANTISSA_BITS)
+#define MAX_RGB9E5_MANTISSA          (RGB9E5_MANTISSA_VALUES-1)
+#define MAX_RGB9E5                   (((float)MAX_RGB9E5_MANTISSA)/RGB9E5_MANTISSA_VALUES * (1<<MAX_RGB9E5_EXP))
+#define EPSILON_RGB9E5               ((1.0/RGB9E5_MANTISSA_VALUES) / (1<<RGB9E5_EXP_BIAS))
+
+typedef struct
+{
+  uint32_t mantissa : 23;
+  uint32_t biasedexponent : 8;
+  uint32_t negative : 1;
+} BitsOfIEEE754;
+
+typedef union
+{
+  uint32_t raw;
+  float value;
+  BitsOfIEEE754 field;
+} float754;
+
+typedef struct
+{
+    uint32_t r : RGB9E5_MANTISSA_BITS;
+    uint32_t g : RGB9E5_MANTISSA_BITS;
+    uint32_t b : RGB9E5_MANTISSA_BITS;
+    uint32_t biasedexponent : RGB9E5_EXPONENT_BITS;
+} BitsOfRGB9E5;
+
+typedef union
+{
+    uint32_t raw;
+    BitsOfRGB9E5 field;
+} rgb9e5;
+
+float ClampRange_for_rgb9e5(float x)
+{
+    if (x > 0.0)
+    {
+        if (x >= MAX_RGB9E5)
+            return MAX_RGB9E5;
+    }
+    else
+    {
+        /* NaN gets here too since comparisons with NaN always fail! */
+        return 0.0f;
+    }
+
+    return x;
+}
+
+float Max(float a, float b)
+{
+    return a >= b ? a : b;
+}
+
+float MaxOf3(float x, float y, float z)
+{
+    return Max(Max(x, y), z);
+}
+
+/* Ok, FloorLog2 is not correct for the denorm and zero values, but we
+   are going to do a max of this value with the minimum rgb9e5 exponent
+   that will hide these problem cases. */
+int FloorLog2(float x)
+{
+  float754 f;
+
+  f.value = x;
+  return (f.field.biasedexponent - 127);
+}
+
+int Max(int x, int y)
+{
+  if (x > y) {
+    return x;
+  } else {
+    return y;
+  }
+}
+
+rgb9e5 float3_to_rgb9e5(const float r, const float g, const float b)
+{
+  rgb9e5 retval;
+  float maxrgb;
+  int rm, gm, bm;
+  float rc, gc, bc;
+  int exp_shared;
+  double denom;
+
+  rc = ClampRange_for_rgb9e5(r);
+  gc = ClampRange_for_rgb9e5(g);
+  bc = ClampRange_for_rgb9e5(b);
+
+  maxrgb = MaxOf3(rc, gc, bc);
+  exp_shared = Max(-RGB9E5_EXP_BIAS-1, FloorLog2(maxrgb)) + 1 + RGB9E5_EXP_BIAS;
+  assert(exp_shared <= RGB9E5_MAX_VALID_BIASED_EXP);
+  assert(exp_shared >= 0);
+  /* This pow function could be replaced by a table. */
+  denom = pow(2, exp_shared - RGB9E5_EXP_BIAS - RGB9E5_MANTISSA_BITS);
+
+  int maxm = (int) floor(maxrgb / denom + 0.5f);
+  if (maxm == MAX_RGB9E5_MANTISSA+1) {
+    denom *= 2;
+    exp_shared += 1;
+    assert(exp_shared <= RGB9E5_MAX_VALID_BIASED_EXP);
+  } else {
+    assert(maxm <= MAX_RGB9E5_MANTISSA);
+  }
+
+  rm = (int) floor(rc / denom + 0.5);
+  gm = (int) floor(gc / denom + 0.5);
+  bm = (int) floor(bc / denom + 0.5);
+
+  assert(rm <= MAX_RGB9E5_MANTISSA);
+  assert(gm <= MAX_RGB9E5_MANTISSA);
+  assert(bm <= MAX_RGB9E5_MANTISSA);
+  assert(rm >= 0);
+  assert(gm >= 0);
+  assert(bm >= 0);
+
+  retval.field.r = rm;
+  retval.field.g = gm;
+  retval.field.b = bm;
+  retval.field.biasedexponent = exp_shared;
+
+  return retval;
+}
+////////
 
 int main(int argc, char* argv[])
 {
@@ -216,11 +351,13 @@ int main(int argc, char* argv[])
     unsigned int Nlevels;
     for (Nlevels = 1; (imageInput.width() >> Nlevels) > 0; ++Nlevels);
 
+    uint32_t* dataOut = new uint32_t[x * y];
+
     // borders
     for (unsigned int level = 0; level < Nlevels; ++level)
     {
         stringstream filenameOutput (stringstream::in | stringstream::out);
-        filenameOutput << filename << "_filtered_" << level << ".hdr";
+        filenameOutput << filename << "_filtered_" << level << ".png";
 
         cout << "processing file " << filenameOutput.str() << endl;
         unsigned int width = imageInput.width() >> level;
@@ -237,13 +374,18 @@ int main(int argc, char* argv[])
         for (int j = 0; j < imageOutput.height(); ++j)
         for (int i = 0; i < imageOutput.width();  ++i)
         {
-            data[offset++] = imageOutput(i, j, 0, 0);
-            data[offset++] = imageOutput(i, j, 0, 1);
-            data[offset++] = imageOutput(i, j, 0, 2);
+            rgb9e5 value = float3_to_rgb9e5(
+                imageOutput(i, j, 0, 0),
+                imageOutput(i, j, 0, 1),
+                imageOutput(i, j, 0, 2));
+
+            dataOut[offset++] = value.raw;
         }
 
-        stbi_write_hdr(filenameOutput.str().c_str(), width, height, 3, data);
+        stbi_write_png(filenameOutput.str().c_str(), width, height, 4, dataOut, 0);
     }
+
+    delete[] dataOut;
 
     return 0;
 }
